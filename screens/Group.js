ea3235a0e0d29446642d70c22147c83d2887e273
João Vitor Chaves <joaovmanciochaves@gmail.com>
2023-10-04 19:34:05 -0300

    uptade with edit mode

--- screens/Group.js
+++ screens/Group.js
@@ -81,17 +81,6 @@ export default function Group({ navigation }) {
         return () => backHandler.remove()
     }, [navigation, editMode])
 
-    return (
-        <View style={{...main.backgroundScreens, ...main.container}}>
-            <ModalCreateGroup visible={createGroupModalVisible} setVisible={setCreateGroupModalVisible}/>
-            <View style={styles.header}>
-                <View style={styles.photoContainer}>
-                    <TouchableOpacity onPress={() => {navigation.navigate('UserPhoto')}}>
-                        <View style={styles.photo}>
-                            <View style={styles.online}></View>
-                        </View>
-                    </TouchableOpacity>
-                    <Text style={{...main.mainText}}>Welcome, User!</Text>
     if (!editMode) {
         return (
             <View style={{...main.backgroundScreens, ...main.container}}>
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,73 +1,62 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Text, View, StyleSheet, TextInput} from "react-native";
-import ResponsiveButton from '../components/ResponsiveButton.js';
 
-=======
-import { Text, View, StyleSheet, TextInput} from "react-native"
-import ResponsiveButton from "../components/ResponsiveButton.js"
->>>>>>> userPhoto
-=======
-import { Text, View, StyleSheet, TextInput} from "react-native"
-import ResponsiveButton from "../components/ResponsiveButton.js"
 import { Text, View, StyleSheet, TextInput} from "react-native";
 import ResponsiveButton from '../components/ResponsiveButton.js';
 import { StatusBar } from 'expo-status-bar';
 
->>>>>>> 36ff0f6519d4aa844ca4993906195a83284c3a5e
 import main from "../styles/Main.js"
 
 export default function SignUp({ navigation }) {
     console.log({...main.backgroundScreens})
     return (
         <View style={{...main.backgroundScreens, ...main.container}}>
-        <StatusBar style='light' />
-        <View style={styles.header}>
-            <Text style={{...main.title}}>
-                Security System
-            </Text>
-            <Text style={{...main.secondaryText, textAlign: 'center'}}>
-                SIGN UP
-            </Text>
-        </View>
-        <View style={{...main.form}}>
-            <View style={{...main.formArea}}>
-                <Text style={{...main.secondaryText}}>Username</Text>
-                <TextInput style={{...main.textInput, textAlign: 'center'}} placeholder="Username" />
-            </View>
-            <View style={{...main.formArea}}>
-                <Text style={{...main.secondaryText}}>E-mail</Text>
-                <TextInput style={{...main.textInput, textAlign: 'center'}} placeholder="E-mail" />
-            </View>
-            <View style={{...main.formArea}}>
-                <Text style={{...main.secondaryText}}>Password</Text>
-                <TextInput style={{...main.textInput, textAlign: 'center'}} placeholder="Password" secureTextEntry={true} />
+            <StatusBar style='light' />
+            <View style={styles.header}>
+                <Text style={{...main.title}}>
+                    Security System
+                </Text>
+                <Text style={{...main.secondaryText, textAlign: 'center'}}>
+                    SIGN UP
+                </Text>
             </View>
             <View style={{...main.form}}>
-                <View style={styles.formArea}>
-                    <Text style={{...main.secondaryText}}>Fullname</Text>
-                    <TextInput placeholder="Fullname" />
+                <View style={{...main.formArea}}>
+                    <Text style={{...main.secondaryText}}>Username</Text>
+                    <TextInput style={{...main.textInput, textAlign: 'center'}} placeholder="Username" />
                 </View>
-                <View style={styles.formArea}>
-                    <Text style={{...main.secondaryText}}>CPF</Text>
-                    <TextInput placeholder="CPF" />
-                </View>
-                <View style={styles.formArea}>
+                <View style={{...main.formArea}}>
                     <Text style={{...main.secondaryText}}>E-mail</Text>
-                    <TextInput placeholder="E-mail" />
+                    <TextInput style={{...main.textInput, textAlign: 'center'}} placeholder="E-mail" />
                 </View>
-                <View style={styles.formArea}>
+                <View style={{...main.formArea}}>
                     <Text style={{...main.secondaryText}}>Password</Text>
-                    <TextInput placeholder="Password" secureTextEntry={true} />
+                    <TextInput style={{...main.textInput, textAlign: 'center'}} placeholder="Password" secureTextEntry={true} />
                 </View>
-                <View style={styles.formArea}>
-                    <ResponsiveButton text="Confirm" callback={() => navigation.navigate("UserPhoto")} />
+                <View style={{...main.form}}>
+                    <View style={styles.formArea}>
+                        <Text style={{...main.secondaryText}}>Fullname</Text>
+                        <TextInput placeholder="Fullname" />
+                    </View>
+                    <View style={styles.formArea}>
+                        <Text style={{...main.secondaryText}}>CPF</Text>
+                        <TextInput placeholder="CPF" />
+                    </View>
+                    <View style={styles.formArea}>
+                        <Text style={{...main.secondaryText}}>E-mail</Text>
+                        <TextInput placeholder="E-mail" />
+                    </View>
+                    <View style={styles.formArea}>
+                        <Text style={{...main.secondaryText}}>Password</Text>
+                        <TextInput placeholder="Password" secureTextEntry={true} />
+                    </View>
+                    <View style={styles.formArea}>
+                        <ResponsiveButton text="Confirm" callback={() => navigation.navigate("UserPhoto")} />
+                    </View>
+                    <View style={{...main.formArea}}>
+                        <ResponsiveButton text="Confirm" callback={() => navigation.navigate('Group')}/>
+                    </View>
                 </View>
-            <View style={{...main.formArea}}>
-                <ResponsiveButton text="Confirm" callback={() => navigation.navigate('Group')}/>
             </View>
         </View>
-    </View>
     )
 }
 
