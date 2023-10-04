1d7b25a2699dd5015f7d394472cb22d00e3636ac
GabrielRodriguesDeveloper <gabrielsousa455@gmail.com>
2023-10-03 21:01:08 -0300

    update

--- App.js
+++ App.js
@@ -6,7 +6,8 @@ import * as Font from 'expo-font';
 
 import Home from './screens/Home.js';
 import Login from './screens/Login.js';
-import Group from './screens/Group.js';
+import Group from './screens/Group.js';;
+import SignUp from './screens/SignUp.js';
 
 const Stack = createNativeStackNavigator()
 
@@ -32,6 +33,7 @@ export default function App() {
           <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
           <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
           <Stack.Screen name="Group" component={Group} options={{ headerShown: false }} />
+          <Stack.Screen name="SignUp" component={SignUp} options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     )
